6384e54d71cec8da9f534b5c1cc3630f1b2b8efe
Andrey Sidorenko <spintronic@tuta.io>
2018-03-26 20:07:32 +0200

    Add the decoding function

--- One Time Pad/one_time_pad.py
+++ One Time Pad/one_time_pad.py
@@ -18,7 +18,7 @@ def encode_message(msg, key, nbits):
         key = [randint(0,1) for i in range(len(msg_in_bits))]
     else:
         if len(key) < len(msg_in_bits):
-            raise NameError("The key should have the same size as, or longer than, the message.")
+            raise ValueError("The key should have the same size as, or longer than, the message.")
         else:
             key = list(map(int, key))
     
@@ -35,7 +35,7 @@ def message_to_bits(msg, nbits):
     msg_in_bits = []
     for char in msg:
         msg_in_bits += int_to_bits(char_to_int(char), nbits)
-        
+
     return msg_in_bits.copy()
 
 def int_to_bits(n, pad):
@@ -45,13 +45,13 @@ def int_to_bits(n, pad):
     Returns an array
     """
     result = [0] * pad
-    pos = 0
+    pos = pad - 1
     
     while n > 0:
         result[pos] = (0 if n % 2 == 0 else 1)
 
         n = n//2
-        pos += 1
+        pos -= 1
         
     return result.copy()
 
@@ -59,3 +59,41 @@ def char_to_int(char):
     
     return ord(char)
 
+      
+def decode_msg(msg,key,nbits):
+    """
+    The function to decrypt the message
+    Takes three arguments:
+        string msg - a message to be decoded
+        string key - a key with the length len(msg) * nbits. If key == None
+                     the function finds a pseudorandom key
+        integer nbits - number of bits for each symbol in the message
+    Returns an decrypted message as a string
+    """
+    decoded_msg = ""
+    msg_in_bits = [int(msg[i])^int(key[i]) for i in range(len(msg))]
+    for n in range(len(msg_in_bits)//nbits):
+        pad = msg_in_bits[n * 7: (n + 1) *7]
+        char_code = bits_to_int(pad,nbits)
+        decoded_msg += int_to_char(char_code)
+    
+    return decoded_msg
+
+def bits_to_int(msg_in_bits, nbits):
+    """
+    The fucntion to conver a binary representation to decimal
+    """
+    loc = nbits - 1
+    code = 0
+    for bit in msg_in_bits:
+        if bit == 1:
+            code += 2**loc
+        loc -= 1
+    return code
+        
+def int_to_char(code):
+    return chr(code)
+
+enc_msg = encode_message(msg = 'AB', key = None, nbits = 7)
+dec_msg = decode_msg(enc_msg['msg'],enc_msg['key'],7)
+print(dec_msg)
